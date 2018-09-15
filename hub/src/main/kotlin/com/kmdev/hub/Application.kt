425e27ec9926141a1cab121f97a75306df99b98d
Keuller Magalhaes <keuller.magalhaes@gmail.com>
2018-09-15 18:29:41 -0300

    Bug fixed: Moquette server couldn't starts.

--- hub/src/main/kotlin/com/kmdev/hub/Application.kt
+++ hub/src/main/kotlin/com/kmdev/hub/Application.kt
@@ -3,6 +3,7 @@ package com.kmdev.hub
 import io.vertx.reactivex.core.RxHelper
 import io.vertx.reactivex.core.Vertx
 import org.slf4j.LoggerFactory
+import java.util.concurrent.TimeUnit
 
 class Application
 
@@ -16,8 +17,9 @@ fun main(args: Array<String>) {
         vertx.close()
     })
 
-    RxHelper.deployVerticle(vertx, ProxyVerticle())
-            .concatWith { RxHelper.deployVerticle(vertx, MessagingVerticle()) }
+    RxHelper.deployVerticle(vertx, MessagingVerticle())
+            .concatWith { RxHelper.deployVerticle(vertx, ProxyVerticle()) }
+            .delay(1L, TimeUnit.SECONDS)
             .count()
             .doOnError { err -> log.error(err.message) }
             .doOnSuccess { _ -> log.info("Hub module has been started.") }
